79d83a4bc4956ad9e1399cdbf08b05fa4f0d6ed6
787378146@qq.com <twgknuic18>
2017-06-02 17:32:51 +0800

    链表操作提交

--- src/lintcode/LinkedList.java
+++ src/lintcode/LinkedList.java
@@ -7,13 +7,16 @@ public class LinkedList {
     public static void main(String[] args) {
         ListNode head = new ListNode(1);
         head.next = new ListNode(2);
-        ListNode node = head.next.next = new ListNode(3);
-        node.next = null;
-        node = removeElements(head, 3);
-        while (node != null) {
-            System.out.println(node.val);
-            node = node.next;
+        head.next.next = new ListNode(3);
+        printNode(reverse(head));
+    }
+
+    public static void printNode(ListNode head) {
+        while (head != null) {
+            System.out.print(head.val + "->");
+            head = head.next;
         }
+        System.out.println("null");
     }
 
     public static void deleteNode(ListNode node) {
@@ -23,7 +26,53 @@ public class LinkedList {
     }
 
     /**
+     * 35-翻转链表
+     * @param head
+     * @return
+     */
+    public static ListNode reverse(ListNode head) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+        ListNode node = null;
+        ListNode next = head;
+        ListNode third = next.next;
+        while (true) {
+            next.next = node;
+            node = next;
+            next = third;
+            third = next.next;
+            if (third == null) {
+                break;
+            }
+        }
+        next.next = node;
+        return next;
+    }
+
+    /**
+     * 112-删除排序链表中的重复元素
+     * @param head
+     * @return
+     */
+    public static ListNode deleteDuplicates(ListNode head) {
+        ListNode node = head;
+        while (node != null) {
+            if (node.next == null) {
+                return head;
+            }
+            if (node.val == node.next.val) {
+                node.next = node.next.next;
+            } else {
+                node = node.next;
+            }
+        }
+        return head;
+    }
+
+    /**
      * 174-删除链表中倒数第n个节点
+     *
      * @param head
      * @param n
      * @return
@@ -51,6 +100,46 @@ public class LinkedList {
             return head;
         }
         preNode.next = preNode.next.next;
+        return head;
+    }
+
+    /**
+     * 165-合并两个排序链表
+     *
+     * @param l1
+     * @param l2
+     * @return
+     */
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        if (l1 == null) {
+            return l2;
+        }
+        if (l2 == null) {
+            return l1;
+        }
+        ListNode head = null;
+        if (l1.val > l2.val) {
+            head = l2;
+            l2 = l2.next;
+        } else {
+            head = l1;
+            l1 = l1.next;
+        }
+        while (l1 != null && l2 != null) {
+            if (l1.val > l2.val) {
+                head.next = l2;
+                l2 = l2.next;
+            } else {
+                head.next = l1;
+                l1 = l1.next;
+            }
+            head = head.next;
+        }
+        if (l1 != null) {
+            head.next = l1;
+        } else {
+            head.next = l2;
+        }
         return head;
     }
 
